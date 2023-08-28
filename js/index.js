c4a4d7ce7dacb4dd140037f8fe701b0f3234078d
Alexandr <bocsyr.516@gmail.com>
2023-08-28 22:42:30 +0600

    upd #1

--- js/index.js
+++ js/index.js
@@ -10,20 +10,42 @@ const seviceID = 'service_vvopxy9';
 const templateID = 'template_fd9yh8p';
 const publicKey = 'user_UD2cRh45GVcy1v7EZeuWH'
 
+let name = document.getElementById('name')
+let tel = document.getElementById('tel')
+let email = document.getElementById('email')
+
 let submitButton = document.getElementById('submitButton')
-submitButton.addEventListener('click', () => {
+let form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     submitButton.disabled = true
-    let name = document.getElementById('name').value
-    let tel = document.getElementById('tel').value
-    let email = document.getElementById('email').value
-    emailjs.send(seviceID, templateID, {name, tel, email}, publicKey)
+    emailjs
+        .send(
+            seviceID,
+            templateID,
+            {
+                name:name.value,
+                tel: tel.value,
+                email: email.value
+            },
+            publicKey
+        )
         .then(() => {
-            alert('Спасибо за заявку!')
-            document.getElementById('name').value = ''
-            document.getElementById('tel').value = ''
-            document.getElementById('email').value = ''
+            name.value = ''
+            tel.value = ''
+            email.value = ''
             submitButton.disabled = false
-        });
+            alert('Спасибо за заявку!')
+        })
+        .catch( e => {
+            console.log(e)
+            alert("Что-то пошло не так...")
+        })
+
+
+
+    submitButton.disabled = false
 })
 
 
@@ -44,7 +66,6 @@ for (let i=0; i<=accordionsItems.length-1; i++) {
 
 let thirdQuest = document.getElementsByClassName('thirdQuest')[0]
 let thirdQuestItems = thirdQuest.querySelectorAll('.thirdQuest__item')
-console.log(thirdQuest)
 
 for (let i=0; i<thirdQuestItems.length; i++) {
     thirdQuestItems[i].querySelector('.itemNumber').innerHTML = '.0'+(i+1)
